16d45bf9757482ff73fb558e875de0abb57ec56d
SzymonPL188 <rogalski.szymek@gmail.com>
2024-03-21 19:27:16 +0100

    queue

--- cukiernia/queue.py
+++ cukiernia/queue.py
@@ -18,7 +18,29 @@ def push_to_queue(order: Order, queue_name):
 
 
 def remove_from_queue(order: Order, queue_name):
-    pass
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue_name)
+
+    channel.basic_publish(exchange='',
+                      routing_key=queue_name,
+                      body=order.__repr__())
+    
+    connection.close()
 
 def get_from_queue(queue_name) -> Order:
-    pass
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue_name)
+
+    method_frame, header_frame, body = channel.basic_get(queue=queue_name)
+
+    connection.close()
+
+    return Order.from_repr(body.decode('utf-8'))
+
+
