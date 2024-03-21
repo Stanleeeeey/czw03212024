56389c5defab1134f371e73d56612511344a42c6
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2024-03-21 19:25:35 +0100

    DataBase class

--- cukiernia/orders_realization.py
+++ cukiernia/orders_realization.py
@@ -1,4 +1,15 @@
-import sqlalchemy
-import models
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import *
 
-#TODO: magicxzne robienie ciasta
+
+class DataBase:
+
+    def __init__(self):
+        engine = create_engine('sqlite:///cukiernia.db', echo=True if __name__ == "__main__" else False)
+        db.metadata.create_all(engine)
+        self.Session = sessionmaker(bind=engine)
+
+    def get_by_id(self, id:int):
+        session = self.Session()
+
